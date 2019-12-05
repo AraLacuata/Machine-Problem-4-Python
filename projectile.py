8a451506cba48cdf170753bba5db223708627962
AraLacuata <57554064+AraLacuata@users.noreply.github.com>
2019-12-05 21:02:04 +0800

    Add files via upload

---
+++ projectile.py
@@ -0,0 +1,41 @@
+import math as math
+import numpy as np
+import matplotlib.pyplot as plt
+ho = float(input('Initial height: '))
+vo = float(input('Initial velocity: '))
+th = float(input('Angle in degrees: '))
+ax = float(input('x-component acceleration: '))
+ay = float(input('y-component acceleration: '))
+pr = float(input('Precision(Time Interval): '))
+
+if ay >= 0:
+    print('Positive or no vertical acceleration would result in no free fall. Aborting.')
+    quit()
+    
+th = math.radians(th)
+vox = vo*math.cos(th)
+voy = vo*math.sin(th)
+
+rt = [ay/2, voy, ho]
+tm = max(np.roots(rt))
+d = np.arange(0,tm,pr)
+x = np.zeros(len(d)+1)
+y = np.zeros(len(d)+1)
+
+t = pr
+x[0] = 0
+y[0] = ho
+
+n = np.arange(0,len(d)-1,1)
+for i in n:
+    xt = (ax*(t**2))/2 + vox*t
+    yt = (ay*(t**2))/2 + voy*t + ho
+    x[i+1] = xt
+    y[i+1] = yt
+    t=t+pr
+    
+x[len(d)] = (ax/2)*tm**2 + vox*tm
+y[len(d)] = 0
+
+plt.plot(x,y)
+plt.grid()
